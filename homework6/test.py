82645348d6106996e2c52a431e499477ee23110c
Usagich <liloddy@gmail.com>
2017-12-12 10:29:18 +0200

    Update test.py

--- homework6/test.py
+++ homework6/test.py
@@ -7,7 +7,6 @@ class TestUM(unittest.TestCase):
         pass
 
     def test_dict(self):
-        '''Check that dictionary is not empty'''
         self.assertTrue(info.pf)
 
 if __name__ == '__main__':
