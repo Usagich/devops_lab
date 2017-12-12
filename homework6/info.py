cf576d0a933de9e7e2c4da28ec4cd173c1ae05ae
Usagich <liloddy@gmail.com>
2017-12-12 10:29:59 +0200

    Update info.py

--- homework6/info.py
+++ homework6/info.py
@@ -2,10 +2,22 @@ import sys
 import subprocess
 
 
-print("python ver.", sys.version)
-print(subprocess.getoutput("pyenv version-name"))
-print("where is python bin:", sys.executable)
-print("pip location:", subprocess.getoutput("which pip"))
-print("virtual env:", sys.exec_prefix)
-print("site-packages location:", next(p for p in sys.path if "site-packages" in p))
-print("packages:\n", subprocess.getoutput("pip freeze"))
+class PythonFields:
+    def __init__(self):
+        self.version = sys.version
+        self.pyenv = subprocess.getoutput("pyenv version-name")
+        self.pythonBin = sys.executable
+        self.pipLocal = subprocess.getoutput("which pip")
+        self.virtualEnv = sys.exec_prefix
+        self.sitePackages = next(p for p in sys.path if "site-packages" in p)
+        self.packages = subprocess.getoutput("pip freeze")
+
+pf = PythonFields()
+
+print("python ver.", pf.version)
+print("pyenv ver.:", pf.pyenv)
+print("where is python bin:", pf.pythonBin)
+print("pip location:", pf.pipLocal)
+print("virtual env:", pf.virtualEnv)
+print("site-packages location:", pf.sitePackages)
+print("packages:\n", pf.packages)
